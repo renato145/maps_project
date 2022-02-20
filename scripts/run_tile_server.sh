b7f2c2fe923374010710e50860ed63981d828f30
Renato <renato145@hotmail.com>
2022-02-21 03:26:47 +1030

    update script

--- scripts/run_tile_server.sh
+++ scripts/run_tile_server.sh
@@ -2,11 +2,11 @@
 set -x
 set -eo pipefail
 
-RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=postgres_osm' --format '{{.ID}}')
+RUNNING_POSTGRES_CONTAINER=$(docker ps --filter 'name=pgosm' --format '{{.ID}}')
 
 docker run --rm -dt \
 	-p 7800:7800 \
 	--link ${RUNNING_POSTGRES_CONTAINER}:pg \
-	-e DATABASE_URL=postgresql://postgres:password@pg/osm \
-	--name "pg_tileserv_$(date '+%s')" \
+	-e DATABASE_URL=postgresql://postgres:password@pg/osm_peru \
+	--name "pg_tileserv" \
 	pramsey/pg_tileserv:latest
